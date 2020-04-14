dc89880893bcb338487ea4d751026d8c393739bc
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-14 14:51:31 -0600

    create action works and info that is sent via postman is showing in SQLiteStudio - moving on to Read action for MVP

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -7,6 +7,22 @@ const db = knex(knexFile.development)
 
 const router = express.Router();
 
+router.post('/', (req, res)=>{
+    const carsData = req.body;
+    db('cars').insert(carsData)
+    .then(ids=>{
+        db('cars').where({ id: ids[0]})
+        .then(newCarEntry =>{
+            res.status(201).json(newCarEntry)
+        })
+    })
+    .catch(error =>{
+        console.log("Server error in post", error)
+        res.status(500).json({error: `Server error durring post ${error.message}`})
+
+    })
+})
+
 
 
 
