3985ff42a2014aea62c3c397bce8cf3ef9a29dab
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-14 14:55:02 -0600

    Read action for all items in data has been made. moving next to a read action for specific ids

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -7,6 +7,17 @@ const db = knex(knexFile.development)
 
 const router = express.Router();
 
+router.get('/', (req, res)=>{
+    db('cars')
+    .then(cars =>{
+        res.status(200).json(cars)
+    })
+    .catch(errors =>{
+        console.log(errors)
+        res.status(500).json({error: error.message})
+    })
+})
+
 router.post('/', (req, res)=>{
     const carsData = req.body;
     db('cars').insert(carsData)
